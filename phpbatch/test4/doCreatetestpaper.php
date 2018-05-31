98e54db112d70133c91d7460ae4a46e91d3dbc22
Abhishek pundir <pundirabhishektehri@gmail.com>
2018-05-31 09:00:27 +0530

    online testing portal

---
+++ phpbatch/test4/doCreatetestpaper.php
@@ -0,0 +1,57 @@
+<?php
+ session_start();
+ if(!isset($_SESSION['username']))
+     header('location:http://localhost/phpbatch/test4/adminlogin.php');
+ if($_SESSION['username']!='admin')
+     header('location:http://localhost/phpbatch/test4/index.html');
+ 
+ $size=sizeof($_POST);
+
+ for($j=1,$i=1;$i<=$size;$i++)
+ {
+  
+  $index="que".$j;
+  if(!isset($_POST[$index]))
+  {
+    $j++;
+	$i--;
+	continue;
+  }
+  else
+  {
+  $j++;
+  $queid[$i]=$_POST[$index];
+
+  }
+ }
+ $con=mysqli_connect('localhost','root');
+ mysqli_select_db($con,'onlinetestdb');
+ 
+  
+ $q="select subject from question where queid=$queid[1]";
+ $result=mysqli_query($con,$q);
+ $row=mysqli_fetch_array($result);
+ $subject=$row['subject'];
+ 
+ $q1="insert into test (subject) values ('$subject')";
+
+ mysqli_query($con,$q1);
+ 
+ 
+ $q2="select max(testid) from test";
+ $result2=mysqli_query($con,$q2);
+ $row2=mysqli_fetch_array($result2);
+ $testid=$row2[0];
+ 
+
+ for($i=1;$i<=$size;$i++)
+ {
+  $q3="insert into test_question (queid,testid) values ($queid[$i],$testid)";
+ mysqli_query($con,$q3);
+ }
+ 
+ 
+ mysqli_close($con);
+ header('location:http://localhost/phpbatch/test4/managetests.php');
+ 
+?>
